316096366df8c25268a86b231d0f06e27be28923
ShafinRME <shafin.rmedu4@gmail.com>
2022-04-22 16:25:19 +0600

    delete method is developing

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -19,6 +20,7 @@ async function run() {
     try {
         await client.connect();
         const userCollection = client.db('foodExpress').collection('user');
+        // Get User
         app.get('/user', async (req, res) => {
             const query = {};
             const cursor = userCollection.find(query);
@@ -26,12 +28,19 @@ async function run() {
             res.send(users);
         })
 
+        // Post user : add a new User
+
         app.post('/user', async (req, res) => {
             const newUser = req.body;
             console.log('adding new user', newUser);
             const result = await userCollection.insertOne(newUser);
             res.send(result)
         });
+        // Delete a user
+        app.delete('/user/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+        })
 
     }
     finally {
